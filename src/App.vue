f384bc0b867d962c5a93fa8be469a0402c05a5a5
Roy Ali Hasan <informativepath0987@gmail.comgit config --global user.email informativepath0987@gmail.com>
2021-10-27 09:20:12 +0500

    "Add child coponent "

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Child user="Roy Ali Hasan" />
   </div>
 </template>
 
 <script>
-
-
+import Child from "./components/Child.vue";
 export default {
-  name: 'App',
-  components: {
-  
-  }
-}
+  name: "App",
+  components: { Child },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Hello from Child</h1>
+    {{ user }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Child",
+  props: ["user"],
+};
+</script>
+
